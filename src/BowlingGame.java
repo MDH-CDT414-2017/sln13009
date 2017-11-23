1e498d6091d8c39e23aed95450aec190e2a83fa3
Sebastian Lindgren <sebbestune@icloud.com>
2017-11-23 15:29:37 +0100

    OpenFramesWork
    So far only open frames work.  (5/11 "example"-based test cases pass.)

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,6 +1,6 @@
 /** BowlingGame Score calculator 
  *
- * @author CDT414 Student: 
+ * @author CDT414 Student: Sebastian Lindgren, sln13009
  * @version 1.0 
  * @date 2016-11-24
  */
@@ -10,18 +10,44 @@ public class BowlingGame {
 	 * @param game Expected format "[n,n][n,n]..[n,n]"
 	 * 
 	 */	 
+	
+	int score;
+	
 	public BowlingGame(String game)
 	{	
-		//TODO: create constructor for BowlingGame
+		this.score = calculateScore(game);
 	}
 	
 	/** getScore method returns a score of current Bowling game or -1 if error
 	 * 
 	 * @return Integer value of Bowling score, in case of error return value is -1 
 	 */
+	
+	private int calculateScore(String arr)
+	{	
+		
+		String[] items = arr.replaceAll("\\[", "").replaceAll("\\]", ",").replaceAll("\\s", "").split(",");
+		
+		int[] frames = new int[items.length];
+
+		for (int i = 0; i < items.length; i++) {
+			try {
+		        frames[i] = Integer.parseInt(items[i]);
+		    } catch (NumberFormatException nfe) {
+		        return -1;
+		    };
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < items.length; i++) {
+			sum = sum + frames[i];
+		}
+		
+		return sum;
+	}
+	
 	public int getScore() {
-		//TODO: calculate the score of game and return correct value
-		return(-1);
+		return score;
 	}
 	
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -15,11 +15,49 @@ public class BowlingGameTest extends TestCase {
 	 * 	
 	 *  If no game is provided, score should be -1 (error)   
 	 */	     
-	public void test01() {
+	public void test01NoGame() {
         BowlingGame bowlingGame = new BowlingGame("");
         assertEquals(-1, bowlingGame.getScore());
     }	
-	
-	//TODO: Add more test cases below...
+	public void test02Open() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(81, bowlingGame.getScore());
+	}
+	public void test03Strike() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(94, bowlingGame.getScore());
+	}
+	public void test04MultipleStrikes() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(112, bowlingGame.getScore());
+	}
+	public void test05Spare() {
+		BowlingGame bowlingGame = new BowlingGame("[1,9][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(88, bowlingGame.getScore());
+	}
+	public void test06MultipleSpares() {
+		BowlingGame bowlingGame = new BowlingGame("[8,2][5,5][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(98, bowlingGame.getScore());
+	}
+	public void test07SpareAfterStrike() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][4,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+        assertEquals(103, bowlingGame.getScore());
+	}
+	public void test08SpareLastFrame() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][7]");
+        assertEquals(90, bowlingGame.getScore());
+	}
+	public void test09StrikeLastFrame() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][10,0][7,2]");
+        assertEquals(92, bowlingGame.getScore());
+	}
+	public void test10SpareThenStrikeLastFrame() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][10]");
+        assertEquals(93, bowlingGame.getScore());
+	}
+	public void test11PerfectGame() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10]");
+        assertEquals(300, bowlingGame.getScore());
+	}
 	
 }
